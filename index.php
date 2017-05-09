11abfca0585ca56bab0c0eafc559e779a5acd9f1
root <root@neuralnetwork.com>
2017-05-09 12:03:52 +0300

    Creating index and added data to nn teach

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+error_reporting( E_ALL );
+define ( 'ROOT_DIR', dirname ( __FILE__ ) );
+
+include "core.php";
+$nn = new NeuralNetwork;
+
+/* Обучение нейронных сетей */
+if(isset($_GET['teach'])){
+	$tonn = array();
+    $nn->addFigure("triangle", "teach/triangle/", $tonn);
+    $nn->addFigure("circle", "teach/circle/", $tonn);
+    $nn->addFigure("square", "teach/square/", $tonn);
+    $ann = $nn->teach($tonn);
+}
+
+echo "template/index.html";
+
 
 ?>
