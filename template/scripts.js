f0cd8f0861158fe37489504c1da2f2e6b72cda38
Zirionneft <zirionneft@gmail.com>
2017-05-11 22:17:14 +0300

    Готовность внешнего вида сайта ~60%; Примерный вид конечного варианта

---
+++ template/scripts.js
@@ -0,0 +1,14 @@
+;(function( $ ) {
+	
+	"use strict";
+
+$(document).ready(function() {
+	$('.bg-image').addClass("slide-bottom");
+	$('.wrapper').addClass('slide-bottom-m');
+	setTimeout(function() {
+		$('.header').addClass('fade-in');
+	},550);
+
+});
+
+})(jQuery);
