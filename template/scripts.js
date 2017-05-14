08f56f38ac04c7eb9d8521f472a8013a40fdd24e
Zirionneft <zirionneft@gmail.com>
2017-05-14 12:39:58 +0300

    Стилизовано поле загрузки изображения

--- template/scripts.js
+++ template/scripts.js
@@ -9,6 +9,23 @@ $(document).ready(function() {
 		$('.header').addClass('fade-in');
 	},550);
 
+	var wrapper = $(".upload-img"),
+        inp = wrapper.find('input');
+    var file_api = (window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+
+    inp.change(function() {
+    	console.log(true);
+        var file_name;
+        if(file_api && inp[0].files[0])
+            file_name = inp[0].files[0].name;
+        else
+            file_name = inp[0].val().replace("C:\\fakepath\\", '');
+
+        if(!file_name.length)
+            return;
+
+        wrapper.find('span').text(file_name);
+    }).change();
 });
 
 })(jQuery);
