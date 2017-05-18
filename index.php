2814b0f9cf5bb6871d519f49879d9c7d9980cb58
Twelvee <redapple54r@gmail.com>
2017-05-18 16:05:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,11 @@ if(isset($_POST['checkfigure'])){
             $answer = "неизвестно";
             break;
     }
-    echo $answer;
+   echo <<<JSCRIPT
+    <br>
+<script>
+$(".result").html('<span class="text">Вероятно, на изображении </span><span class="answer">$answer</span>');
+</script>
+JSCRIPT;
 }
 ?>
